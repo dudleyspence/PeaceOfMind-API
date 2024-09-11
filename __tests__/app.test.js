94556f15a02725a3487fe3cf3ad96663172434f7
dudleyspence <dudleyspence5@gmail.com>
2024-09-12 00:31:24 +0200

    succesfully seeded the database

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,16 @@
-// process.env.NODE_ENV = "test";
+process.env.NODE_ENV = "test";
 
-// const seedData = require("../db/seeds/seed");
-// const mongoose = require("mongoose");
+const seedData = require("../db/seeds/seed");
+const mongoose = require("mongoose");
 
-// beforeAll(() => {
-//   return seedData();
-// });
+beforeAll(() => {
+  return seedData();
+});
 
-// afterAll(() => {
-//   return mongoose.disconnect();
-// });
+afterAll(() => {
+  return mongoose.disconnect();
+});
+
+test("Sample test to ensure tests are working", () => {
+  expect(true).toBe(true);
+});
