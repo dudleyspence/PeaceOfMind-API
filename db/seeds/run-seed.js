b6257e94076a69633283eaef18f64269eff72978
dudleyspence <dudleyspence5@gmail.com>
2024-09-12 09:46:25 +0200

    refactor of the seed file

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,9 +1,11 @@
 const seedData = require("./seed.js");
+const mongoose = require("mongoose");
 
 function runSeed() {
   seedData()
     .then(() => {
       console.log("Seeding process complete.");
+      return mongoose.disconnect();
     })
     .catch((error) => {
       console.error("Seeding failed:", error);
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,17 +1,11 @@
 const { Seeder } = require("mongo-seeding");
-const connectDB = require("../connection.js");
 const dotenv = require("dotenv");
 const path = require("path");
-const mongoose = require("mongoose");
 
 const ENV = process.env.NODE_ENV || "development";
 dotenv.config({ path: `.env.${ENV}` });
 
 const MONGO_URI = process.env.MONGO_URI || process.env.DATABASE_URL;
-
-if (!MONGO_URI) {
-  throw new Error("MongoDB URI is not defined in .env file");
-}
 
 let dataPath;
 
@@ -22,40 +16,26 @@ if (ENV === "test") {
 }
 
 function seedData() {
-  return connectDB()
-    .then(() => {
-      const seeder = new Seeder({
-        database: MONGO_URI,
-        dropDatabase: true,
-      });
-      const resolvedPath = path.resolve(__dirname, dataPath);
+  const seeder = new Seeder({
+    database: MONGO_URI,
+    dropDatabase: true,
+  });
+  const resolvedPath = path.resolve(__dirname, dataPath);
 
-      const collectionReadingOptions = {
-        extensions: ["json"],
-        ejsonParseOptions: {
-          relaxed: false,
-        },
-        transformers: [Seeder.Transformers.replaceDocumentIdWithUnderscoreId],
-      };
+  const collectionReadingOptions = {
+    extensions: ["json"],
+    ejsonParseOptions: {
+      relaxed: false,
+    },
+    transformers: [Seeder.Transformers.replaceDocumentIdWithUnderscoreId],
+  };
 
-      const collections = seeder.readCollectionsFromPath(
-        resolvedPath,
-        collectionReadingOptions
-      );
+  const collections = seeder.readCollectionsFromPath(
+    resolvedPath,
+    collectionReadingOptions
+  );
 
-      return seeder.import(collections);
-    })
-
-    .then(() => {
-      console.log("succesfully seeded the database");
-      return mongoose.disconnect();
-    })
-    .then(() => {
-      console.log("database connection closed");
-    })
-    .catch((error) => {
-      console.error("error seeding database", error);
-    });
+  return seeder.import(collections);
 }
 
 module.exports = seedData;
