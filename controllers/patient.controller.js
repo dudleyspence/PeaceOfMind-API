ff479f458566a9d474e5aa74beb92838a49037a1
dudleyspence <dudleyspence5@gmail.com>
2024-09-23 13:37:12 +0200

    getComments endpoint

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -298,7 +298,7 @@ exports.getCommentsForSpecificDay = (req, res, next) => {
       $lt: endOfDayISO,
     },
   })
-    .populate("User")
+    .populate(author)
     .then((tasks) => {
       res.status(200).send(tasks);
     })
--- models/comment.model.js
+++ models/comment.model.js
@@ -3,9 +3,17 @@ const mongoose = require("mongoose");
 const CommentSchema = new mongoose.Schema(
   {
     text: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, required: true },
     authorType: { type: String, enum: ["Guardian", "Carer"], required: true },
-    patient: { type: mongoose.Schema.Types.ObjectId, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: "authorType",
+      required: true,
+    },
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
   },
   { timestamps: true }
 );
