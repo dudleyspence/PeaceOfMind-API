1abc206051012f5c299e3d923b15b4655b7ef7ef
dudleyspence <dudleyspence5@gmail.com>
2024-09-27 22:59:13 +0200

    updates to schedule update controller

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {
   updateTaskTemplate,
-  updateScheduleTask,
+  updateTaskInstance,
   postTask,
 } = require("../controllers/task.controller");
 
