97e88ac4fe118c9583dcc10ab05060e1d0a521c1
dudleyspence <dudleyspence5@gmail.com>
2024-09-30 00:16:33 +0200

    adds the delete task endpoints

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -12,10 +12,10 @@ router.patch("/tasks/templates/:tasktemplate_id", updateTaskTemplate);
 
 router.post("/tasks", postTask);
 
+router.delete("/tasks/templates/:template_id", deleteTaskTemplate);
+
 router.patch("/tasks/instances/:taskinstance_id", updateTaskInstance);
 
 router.delete("/tasks/instances/:instance_id", deleteTaskInstance);
-
-router.delete("/tasks/templates/:template_id", deleteTaskTemplate);
 
 module.exports = router;
