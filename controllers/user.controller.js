d2155cd8e1251831a9e0c8b60014d283ebd8a4b4
dudleyspence <dudleyspence5@gmail.com>
2024-10-06 00:22:36 +0200

    add user populates both user and patients into guardian and carer

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -24,15 +24,29 @@ exports.addNewUser = (req, res, next) => {
           .then((guardian) => {
             return guardian.populate("user");
           })
-          .then((populatedGuardian) => {
-            console.log("Guardian created:", populatedGuardian);
-            return res.status(200).send(populatedGuardian);
+          .then((populatedGuardianWithUser) => {
+            console.log("Guardian created:", populatedGuardianWithUser);
+            return populatedGuardianWithUser.populate("patients");
+          })
+          .then((populatedGuardianWithPatients) => {
+            console.log("Guardian created:", populatedGuardianWithPatients);
+            return res.status(200).send(populatedGuardianWithPatients);
           });
       } else if (newUser.role === "carer") {
         const newCarer = new Carer({ user: userId });
-        newCarer.save().then((carer) => {
-          return res.status(200).send(carer.populate("user"));
-        });
+        newCarer
+          .save()
+          .then((carer) => {
+            return carer.populate("user");
+          })
+          .then((populatedCarerWithUser) => {
+            return populatedCarerWithUser.populate("patients");
+          })
+          .then((populatedCarerWithPatients) => {
+            console.log("Carer created:", populatedCarerWithPatients);
+
+            return res.status(200).send(populatedCarerWithPatients);
+          });
       } else {
         return res.status(400).send({ message: "Error creating accound type" });
       }
